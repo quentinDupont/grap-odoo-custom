01e918c921f0a7cf3b99dd99404b9473d5b0a2c7
Quentin Dupont <quentin.dupont@grap.coop>
2024-07-08 14:42:25 +0200

    [MIG] grap_change_views_mrp: Migration to 16.0 (from 12.0)

--- grap_change_views_mrp/__manifest__.py
+++ grap_change_views_mrp/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "GRAP - Change Views MRP",
-    "version": "12.0.1.3.2",
+    "version": "16.0.1.0.0",
     "category": "GRAP - Custom",
     "author": "GRAP",
     "website": "https://github.com/grap/grap-odoo-custom",
